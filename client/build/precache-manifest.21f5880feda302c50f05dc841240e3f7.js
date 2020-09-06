b4c569fb47d8d23cbea6fa20b0b46d8fdfe9465d
Skip Khamvongsa <skipk95@gmail.com>
2020-09-06 14:19:06 -0600

    Ran build

---
+++ client/build/precache-manifest.21f5880feda302c50f05dc841240e3f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e14a9a187344a76cdbb462c6a9ddf7c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37b2502035fe112cdba8",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "46097a78dd4240c6f55a",
+    "url": "/static/js/2.df1f00ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.df1f00ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37b2502035fe112cdba8",
+    "url": "/static/js/main.67b90045.chunk.js"
+  },
+  {
+    "revision": "7b283fd047ad48499f66",
+    "url": "/static/js/runtime-main.fac4c79e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
