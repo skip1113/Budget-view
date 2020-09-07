7220850bccdb44b4924b8112b38d6f99b8fb6cbc
Skip Khamvongsa <skipk95@gmail.com>
2020-09-07 15:58:49 -0600

    Router package, Routed additional pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,31 @@
 import React, { useState } from 'react';
 import { IntlProvider } from 'react-intl';
+import { Router, Route } from 'react-router-dom';
 import Layout from './pages/Layout';
+import history from './history';
+import Info from './pages/Info';
+import Saving from './pages/Saving';
+import Spending from './pages/Spending';
+import Tipcalc from './pages/Tipcalc';
+import Tips from './pages/Tips';
 import './styles/App.scss';
 
 function App() {
   const [locale, setLocale] = useState('en');
   return (
-    <IntlProvider locale={locale}>
-      <Layout setLocale={setLocale} />
-    </IntlProvider>
+    <Router history={history}>
+      <Route path="/">
+        <IntlProvider locale={locale}>
+          <Layout setLocale={setLocale} />
+        </IntlProvider>
+      </Route> 
+      <Route path="/spending" component={Spending} />
+      <Route path="/savings" component={Saving} />
+      <Route path="/tipcalc" component={Tipcalc} />
+      <Route path="/info" component={Info} />
+      <Route path="/tips" component={Tips} />
+    </Router>
+
   );
 }
 
