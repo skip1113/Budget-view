5476021b94c2adbc09b526e939dcc5ed4a0e4824
Skip Khamvongsa <skipk95@gmail.com>
2020-12-09 20:39:45 -0700

    Created input boiler plate

--- client/src/components/buttons/Linkbtn.jsx
+++ client/src/components/buttons/Linkbtn.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 
-const Linkbtn = ({ href, icon, title, className, buttonStyle }) => {
-    return (
-        <button
-            style={buttonStyle}
-            onClick={() => window.open(href, '_blank')}
-            title={title}
-        >
-            <img src={icon} alt={title} className={className} />
-        </button>
-    );
-};
+// const Linkbtn = ({ href, icon, title, className, buttonStyle }) => {
+//     return (
+//         <button
+//             style={buttonStyle}
+//             onClick={() => window.open(href, '_blank')}
+//             title={title}
+//         >
+//             <img src={icon} alt={title} className={className} />
+//         </button>
+//     );
+// };
 
-export default Linkbtn;
+// export default Linkbtn;
+export function Input(props) {
+    return (
+      <div className="form-group">
+        <input className="form-control" {...props} />
+      </div>
+    );
+  }
